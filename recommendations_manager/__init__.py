c303d30a2dc85ce1499a119c57c076a54428c481
Oliver De Jesús Rosario Reyes <under-blade500@hotmail.com>
2020-09-28 21:20:58 -0400

    Using np.random.choice to sample with no replacement

--- recommendations_manager/__init__.py
+++ recommendations_manager/__init__.py
@@ -1,11 +1,11 @@
 """ It's in charge of managing communication between the api and the recommerder algoriths """
 
 import os
-from random import choices
 import pickle
 from collections import defaultdict
-import pandas as pd
 from typing import List, Dict
+from numpy.random import choice
+import pandas as pd
 from algorithms import *
 
 
@@ -17,13 +17,14 @@ def obtain_recommendations(names: List[str]) -> Dict[str, List[str]]:
     recom = defaultdict(list)
     for name in names:        
         try:
-            #get the codes of the recommendations from the algorithm
+            #get the codes of the recommendations from the algorithms
             similarity_recommendations = recommender_algorithms["similarity_search"](
                 name.strip())
             clustering_recommendations = recommender_algorithms["soft_clustering"](name.strip())
+            print(set(similarity_recommendations+clustering_recommendations))
             recommendation_codes = list(
                 set(similarity_recommendations+clustering_recommendations))
-            recommendation_codes = choices(recommendation_codes, k=5)
+            recommendation_codes = choice(recommendation_codes, 5, replace=False)
             #use the codes to find the extra information of each anime        
             recommendations_info = [anime_info_df.loc[anime_info_df["code"] == code][relevant_fields].to_dict(
                 'records')[0] for code in recommendation_codes]
