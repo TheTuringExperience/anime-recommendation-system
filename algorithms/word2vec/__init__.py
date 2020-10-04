3b3c747b2bd82cd81338b50678dfa344c150d86d
Oliver De Jesús Rosario Reyes <under-blade500@hotmail.com>
2020-10-04 15:13:43 -0400

    added exception handling

--- algorithms/word2vec/__init__.py
+++ algorithms/word2vec/__init__.py
@@ -9,21 +9,22 @@ with open('./algorithms/word2vec/w2v_cosine_sim.data', 'rb') as filehandle:
 df = pd.read_pickle("./algorithms/word2vec/review_df.pkl") 
 
 def similarity_recommendator(title: str) -> List[str]:
-    
-    # taking the title and rating to store in new data frame called animes
-    animes = df[['name', 'code']]
+    try:
+        # taking the title and rating to store in new data frame called animes
+        animes = df[['name', 'code']]
 
-    #Reverse mapping of the index
-    indices = pd.Series(df.index, index = df['name']).drop_duplicates()# Recommending the Top 5 similar animes
-    # drop all duplicate occurrences of the labels 
-    indices = indices.groupby(indices.index).first()
+        #Reverse mapping of the index
+        indices = pd.Series(df.index, index = df['name']).drop_duplicates()# Recommending the Top 5 similar animes
+        # drop all duplicate occurrences of the labels 
+        indices = indices.groupby(indices.index).first()
 
-    idx = indices[title]
-    sim_scores = sorted(list(enumerate(cosine_similarities[idx])), key = lambda x: x[1], reverse = True)
-    sim_scores = [score for score in sim_scores[0:6] if score[0] != idx]
-    
+        idx = indices[title]
+        sim_scores = sorted(list(enumerate(cosine_similarities[idx])), key = lambda x: x[1], reverse = True)
+        sim_scores = [score for score in sim_scores[0:6] if score[0] != idx]
+        
+        anime_indices = [i[0] for i in sim_scores]
+        recommendations = animes.iloc[anime_indices]["code"].tolist()
 
-    anime_indices = [i[0] for i in sim_scores]
-    recommend = animes.iloc[anime_indices]
-
-    return recommend['code'].tolist()        
+        return recommendations
+    except:
+        return []
