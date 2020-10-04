cb17272eb85745077d383c935bc8c79263332b10
Oliver De Jesús Rosario Reyes <under-blade500@hotmail.com>
2020-10-04 15:10:29 -0400

    Added the recommedentions by rows

--- algorithms/soft_clustering/__init__.py
+++ algorithms/soft_clustering/__init__.py
@@ -6,10 +6,13 @@ import pandas as pd
 anime_genres_df = pd.read_pickle("./algorithms/soft_clustering/anime_genres_df.pkl")
 
 def soft_clustering_recommendator(anime_name: str) -> List[int]:
-    df = anime_genres_df.copy()
-    anime_data = df.loc[anime_genres_df["name"] == anime_name]
-    df["similarity"] = df.genres.map(
-        lambda x: np.dot(x, anime_data["genres"].values[0]))
-    df = df.sort_values(by=["premiered", "similarity"], ascending=False)
-    recommendations = df.iloc[:5].code.tolist()    
-    return recommendations
+    try:
+        df = anime_genres_df.copy()
+        anime_data = df.loc[anime_genres_df["name"] == anime_name]
+        df["similarity"] = df.genres.map(
+            lambda x: np.dot(x, anime_data["genres"].values[0]))
+        df = df.sort_values(by=["premiered", "similarity"], ascending=False)
+        recommendations = df.iloc[:5].code.tolist()    
+        return recommendations
+    except:    
+        return []
--- recommendations_manager/__init__.py
+++ recommendations_manager/__init__.py
@@ -8,29 +8,23 @@ from numpy.random import choice
 import pandas as pd
 from algorithms import *
 
-
 anime_info_df = pd.read_csv("data/anime_data.csv", encoding="utf-8")
 
 relevant_fields = ['full_title', 'code', 'score', 'image_url', 'synopsis', 'type']
 
-def obtain_recommendations(names: List[str]) -> Dict[str, List[str]]:    
-    recom = defaultdict(list)
-    for name in names:        
-        try:
-            #get the codes of the recommendations from the algorithms
-            similarity_recommendations = recommender_algorithms["similarity_search"](
-                name.strip())
-            clustering_recommendations = recommender_algorithms["soft_clustering"](name.strip())
-            print(set(similarity_recommendations+clustering_recommendations))
-            recommendation_codes = list(
-                set(similarity_recommendations+clustering_recommendations))
-            recommendation_codes = choice(recommendation_codes, 5, replace=False)
-            #use the codes to find the extra information of each anime        
-            recommendations_info = [anime_info_df.loc[anime_info_df["code"] == code][relevant_fields].to_dict(
-                'records')[0] for code in recommendation_codes]
-            recom.update({name: recommendations_info})
-            
-        except:
-            continue 
-                    
+def obtain_recommendations(name: str) -> Dict[str, List[str]]:
+    recom = defaultdict(list)    
+    #get the codes of the recommendations from the algorithms and then get the info about them
+    similar = get_info_from_code(recommender_algorithms["similarity_search"](name.strip()))
+    hot = get_info_from_code(recommender_algorithms["soft_clustering"](name.strip()))
+    
+    #assing each group of recommendations to its respective row
+    recom["similarly_described"] = similar
+    recom["hot"] = hot
+
     return {"recommendations":recom}
+
+def get_info_from_code(codes: List[int]):
+    recommendations_info = [anime_info_df.loc[anime_info_df["code"] == code][relevant_fields].to_dict(
+        'records')[0] for code in codes]
+    return recommendations_info
