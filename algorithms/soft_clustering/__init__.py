c5f7219a631503a87a380a8235ca23869496b1a8
oliver rosario <oliverros42@gmail.com>
2020-10-24 17:23:40 -0400

    Remove the inserted anime from the recommendation options

--- algorithms/soft_clustering/__init__.py
+++ algorithms/soft_clustering/__init__.py
@@ -7,8 +7,10 @@ anime_genres_df = pd.read_pickle("./algorithms/soft_clustering/anime_genres_df.p
 
 def soft_clustering_recommendator(anime_name: str, raking_parameter: str) -> List[int]:
     try:
-        df = anime_genres_df.copy()
-        anime_data = df.loc[anime_genres_df["name"] == anime_name]
+        df = anime_genres_df.copy(deep=True)
+        anime_data = df.loc[df["name"] == anime_name]
+        df.drop(anime_data.index, axis=0, inplace=True) #Drop the anime to make sure it's not there
+
         df["similarity"] = df.genres.map(
             lambda x: np.dot(x, anime_data["genres"].values[0]))
         df = df.sort_values(by=[str(raking_parameter), "similarity"], ascending=False)
