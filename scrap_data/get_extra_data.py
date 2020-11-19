0b281ba44848ae1e579d279cac4d103de8cfbedd
oliver rosario <oliverros42@gmail.com>
2020-11-18 21:54:24 -0400

    Reimplemented the anime scrapping scheme

--- scrap_data/get_extra_data.py
+++ scrap_data/get_extra_data.py
@@ -4,6 +4,8 @@ import os
 import time
 import logging
 from typing import Dict, List
+import json
+
 import pandas as pd
 
 from jikanpy import Jikan
@@ -15,73 +17,68 @@ jikan = Jikan()
 
 animes_df = pd.read_csv("../data/anime_codes.csv", encoding="utf-8")
 
-def extract_fields(response: Dict) -> List:
-    fields = [response.get("image_url", ""), response.get("synopsis", ""), 
-                ";;".join([response.get("title_english", "")] + response.get("title_synonyms", [])),
-                response.get("popularity", ""), response.get("members", ""), response.get("scored_by", ""),
-                response.get("type", ""), response.get("rating", ""), response.get("premiered", ""),             
-                ";".join([studio.get("name", "") for studio in response.get("studios", [])]),
-                ";".join([genre.get("name", "").lower() for genre in response.get("genres", [])])
-            ]
+def make_request(code: int, sleep_time: int) -> List:
+    # wait before making too many requests as per API guidelines
+    time.sleep(sleep_time)
 
-    return fields
+    response = jikan.anime(code)
+    # remove useless fields
+    usless_fields = ["headers", "jikan_url",
+                     "request_cache_expiry", "request_cached", "request_hash"]
+    for field in usless_fields:
+        response.pop(field, None)
+    
+    #Don't add hentai anime to the data list
+    if "Hentai" in response.get("rating", ""):
+        return []
+    
+    return [response]
 
 def get_extra_information(animes_info: List) -> List:
     data = []
     time_between_requests = 3
 
-    for code, name, score in animes_info:        
+    for code, name, score in animes_info[:2000]:        
         try:
-            response = jikan.anime(code)
-            time.sleep(time_between_requests)  # wait before making too many requests as per API guidelines
             
-            extra_data = extract_fields(response)
-            #Don't add hentai anime to the data list          
-            if "Hentai" in extra_data[7]:
-                continue
-            
-            current_row = [code, name, score] + extra_data
-            data.append(current_row)           
+            data.extend(make_request(code, time_between_requests))
 
         except APIException as e:
             #If myanimelist refuses the connection stop the scrapping and resume some time later
-            logging.error(f"The server did not respond when scrapping {code}")
+            logging.error(f"The server did not respond when scrapping {code}: {str(e)}")
             
             try:
                 print("Retrying after 15 seconds...")
-                time.sleep(15)
-
-                response = jikan.anime(code)
-                extra_data = extract_fields(response)                
-                #Check that the rating does not contain the word "Hentai"
-                if "Hentai" in extra_data[7]:
-                    continue
-
-                current_row = row.tolist() + extra_data
-                data.append(current_row)
+                data.extend(make_request(code, 15))
 
             except APIException as e:
                 #If myanimelist refuses the connection stop the scrapping and resume some time later
-                logging.error(f"The server did not respond again when scrapping {code}")
+                logging.error(f"The server did not respond again when scrapping {code}: {str(e)}")
                 continue 
                 
         except Exception as e:
-            logging.error(f"Problems getting data for {code}")
+            logging.error(f"Problems getting data for {code}: {str(e)}")
             continue
 
     return data
 
-def store_info(animes_info: List):
-    extra_info_df = pd.DataFrame(data=animes_info, columns=["code", "name", "score", "image_url",
-                                                            "synopsis", "show_titles", "popularity",
-                                                            "members", "scored_by", "type", "rating",
-                                                            "premiered", "studios", "genres"])
-    extra_info_df.fillna("Not available", inplace=True)
-    extra_info_df.to_csv("../data/anime_data.csv", mode="w", index=False)
+def store_info(animes_data: List): 
+    filepath = "../data/anime_data.json"
+    # If the file containing the anime data does not exist then create it
+    if not os.path.exists(filepath):
+        with open(filepath, "w", encoding="utf-8") as j:            
+            json.dump(animes_data, j, ensure_ascii=False)        
+    # If it exists then just append to it
+    else:
+        with open("../data/anime_data.json", "r+", encoding="utf-8") as j:
+            file_content = json.load(j)            
+            file_content.extend(animes_data)
+            j.seek(0)
+            json.dump(file_content, j, ensure_ascii=False)
 
 def main():
-    animes_info = get_extra_information(animes_df.to_numpy().tolist())
-    store_info(animes_info)
+    animes_data = get_extra_information(animes_df.to_numpy().tolist())
+    store_info(animes_data)
 
 if __name__ == "__main__":
     main()
