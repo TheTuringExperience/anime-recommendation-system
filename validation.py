49f59997faac2c8d0449bea8d3e9bb5c58d2f9d5
chriskok <pherkok@gmail.com>
2020-11-25 23:01:46 -0500

    Finished preliminary validation pipeline testing

--- validation.py
+++ validation.py
@@ -8,19 +8,35 @@ from algorithms.soft_clustering import soft_clustering_recommendator
 from recommendations_manager import obtain_recommendations, obtain_random_recommendations
 from utils import get_anime_code_from_name
 
-anime_name = 'vinland saga'
+anime_name = 'cowboy bebop'
 anime_code = get_anime_code_from_name(anime_name)
-print(anime_code)
-
-recs = obtain_recommendations(anime_name)
-print(recs)
-
-synopsis_recs = recs["similar_synopsis"]
-print(synopsis_recs)
+print("current anime: {}".format(anime_code))
 
 data = pd.read_csv('data/recommendations/{}.txt'.format(anime_code), sep=",", header=None)
 data.columns = ["mal_id", "relevance", "name"]
-print(data.head())
+
+def print_matches(recs):
+    for item in recs:
+        curr_code = item['code']
+        if (curr_code in data.mal_id.values):
+            match = data.loc[data['mal_id'] == curr_code].iloc[0]
+            print("item: {}, match_name: {}, rec_count: {}".format(curr_code, match['name'], match['relevance']))
+
+
+recs = obtain_recommendations(anime_name)
+
+synopsis_recs = recs["similar_synopsis"]
+print_matches(synopsis_recs)
+
+review_recs = recs["similarly_described"]
+print_matches(review_recs)
+
+hot_recs = recs["hot"]
+print_matches(hot_recs)
+
+popular_recs = recs["beloved"]
+print_matches(popular_recs)
+
 
 
 true_relevance = np.asarray([[10, 0, 0, 1, 5]])
