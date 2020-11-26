3608d04ef82c951ff985588fb6bd16f0f46abc95
chriskok <pherkok@gmail.com>
2020-11-26 09:42:32 -0500

    Added ndcg scoring algorithm for individual algos

--- validation.py
+++ validation.py
@@ -4,7 +4,9 @@ import pandas as pd
 import os
 import sys
 
+from algorithms.word2vec import similarity_recommendator
 from algorithms.soft_clustering import soft_clustering_recommendator
+from algorithms.synopsis_similarity import synopsis_similarity_recommender
 from recommendations_manager import obtain_recommendations, obtain_random_recommendations
 from utils import get_anime_code_from_name
 
@@ -71,9 +73,48 @@ def get_scores(anime_name, verbose=False):
     
     return score_with_ndcg(recs, data)
 
+# Input: name of anime and array of recommended anime codes for that anime
+# Output: ndcg score
+def score_individual_rec(anime_name, rec_array):
+    anime_code = get_anime_code_from_name(anime_name)
+    try:
+        data = pd.read_csv('data/recommendations/{}.txt'.format(anime_code), sep=",", header=None)
+        data.columns = ["mal_id", "relevance", "name"]
+    except FileNotFoundError:
+        print('no recommendations found for {}'.format(anime_code))
+        return {}
+
+    true_rel_array = []
+    curr_score_array = []
+    for curr_code in rec_array:
+        if (curr_code in data.mal_id.values):
+            # match = data.loc[data['mal_id'] == curr_code].iloc[0]
+            curr_score_array.append(1)
+            true_rel_array.append(1)
+        else:
+            curr_score_array.append(1)
+            true_rel_array.append(0)
+
+    ndcg_value = ndcg_score(np.asarray([true_rel_array]), np.asarray([curr_score_array]))
+
+    return ndcg_value
+
+
 def main():
     score_dict = get_scores('cowboy bebop', verbose=False)
     print(score_dict)
+
+    test_array = similarity_recommendator('cowboy bebop')
+    score = score_individual_rec('cowboy bebop', test_array)
+    print(score)
+
+    test_array = soft_clustering_recommendator('cowboy bebop', 'score')
+    score = score_individual_rec('cowboy bebop', test_array)
+    print(score)
+
+    test_array = synopsis_similarity_recommender('cowboy bebop')
+    score = score_individual_rec('cowboy bebop', test_array)
+    print(score)
 
 if __name__ == "__main__":
     main()
