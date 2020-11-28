1c528eb0c93b6af81dc30a04cddec63432a64cbe
chriskok <pherkok@gmail.com>
2020-11-27 22:58:44 -0500

    Added more features to genre_match df

--- scrap_data_anidb/1_scrap_anime.py
+++ scrap_data_anidb/1_scrap_anime.py
@@ -26,33 +26,56 @@ with open("./anidb_ids.txt", "r", encoding="utf-8") as f:
     f.close()
 
 def get_data():
-    xml_data = list()
+    animes_data = list()
     for idx, aid in zip(range(2), anime_ids):
         params["aid"] = aid
         response = get(url=url, params=params)
         
-        time.sleep(time_between_requests)        
-        xml_data.append(response.content.decode("utf-8", "replace"))
+        # time.sleep(time_between_requests)
+
+        root = ET.fromstring(response.content.decode("utf-8", "replace"))
         
-    return xml_data
+        #extract the data for each relevant field
+        genres = list()
+        for child in root.iter("tag"):
+            if int(child.attrib.get("weight", 0)) > 140:
+                genres.append(child.find("name").text)
+        genres = list(map(lambda s: s.lower(), genres))
+
+        similar_anime = list()
+        for child in root.iter("similaranime"):
+            similar_anime.append(child.get("id", ""))
+
+        character_info = list()
+        for child in root.iter("character"):
+            if child.get("type") == "main character in":
+                character_info.append({"id": child.get("id"),
+                                    "name": child.findtext("name"),
+                                    "description": child.findtext("description")})       
+        animes_data.append({"genres": genres, "similar_anime": similar_anime, "characters": character_info})
+
+        logging.info("collected data of: " + str(aid))
+
+    return animes_data
 
 
-def store_info(xml_data: List):
-    filepath = "../data/anime_data_anidb.xml"
+def store_info(animes_data: List):
+    filepath = "../data/anime_data_anidb.json"
     # If the file containing the anime data does not exist then create it
     if not os.path.exists(filepath):
-        with open(filepath, "w", encoding="utf-8") as x:
-            for xml in xml_data:
-                x.writelines(xml+"\n")
+        with open(filepath, "w", encoding="utf-8") as j:
+            json.dump(animes_data, j, ensure_ascii=False)
     # If it exists then just append to it
     else:
-        with open("../data/anime_data_anidb.xml", "a", encoding="utf-8") as x:
-            for xml in xml_data:
-                x.writelines(xml+"\n")
+        with open(filepath, "r+", encoding="utf-8") as j:
+            file_content = json.load(j)
+            file_content.extend(animes_data)
+            j.seek(0)
+            json.dump(file_content, j, ensure_ascii=False)
 
 def main():
-    xml_data = get_data()
-    store_info(xml_data)
+    animes_data = get_data()
+    store_info(animes_data)
 
 if __name__ == "__main__":
     main()
