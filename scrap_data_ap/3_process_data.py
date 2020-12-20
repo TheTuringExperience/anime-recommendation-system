b74b91f8bd6ef1841f7cc2c08192090192cebf1b
oliver rosario <oliverros42@gmail.com>
2020-12-20 13:37:58 -0400

    Refactor anime and character processing
    Updated the anime and character processing to avoid having a character
    in the characters dataset that who's anime has been deleted from the animes dataset

--- scrap_data_ap/3_process_data.py
+++ scrap_data_ap/3_process_data.py
@@ -14,9 +14,17 @@ def load_data() -> List:
     return data
 
 
+def to_df(anime_data: List):
+    df = pd.DataFrame(anime_data)
+    #Remove the animes with no tags or ranking
+    df.dropna(subset=["tags", "rank"], inplace=True)
+    return df
+
+
 def process_charactes_data(anime_data: List):    
     characters_data = []
-    for anime in anime_data:        
+    #Turn each character into an entry in the characters_data list
+    for anime in anime_data:
         url = anime.get("url", "")
         show_characters = anime.get("characters_data", [])
         for character in show_characters:
@@ -26,23 +34,26 @@ def process_charactes_data(anime_data: List):
     df = pd.DataFrame(characters_data)    
     df.dropna(inplace=True)
     df["character_tags"] = df.character_tags.apply(lambda t: ";".join(t))
-    df.to_csv("../data/anime_planet/characters.csv", encoding="utf-8", index=False)
+    df.to_csv("../data/anime_planet/characters_data.csv", encoding="utf-8", index=False)
 
 
-def process_anime_data(anime_data: List):
-    df = pd.DataFrame(anime_data)
-    df.dropna(inplace=True)
+def process_anime_data(df: pd.DataFrame):    
+    #Remove the characters data
     df.drop(["characters_data"], axis=1, inplace=True)
+    #Remove \n character from the rank, titles and tags
     df["rank"] = df["rank"].apply(lambda s: str(s).replace("\n", ""))
     df["alt_titles"] = df.alt_titles.apply(lambda s: str(s).replace("\n", ""))
-    df["tags"] = df["tags"].apply(lambda x: ";".join(list(map(lambda s: s.replace("\n", ""), x))))    
+    df["tags"] = df["tags"].apply(lambda x: ";".join(list(map(lambda s: s.replace("\n", ""), x))))
     
     df.to_csv("../data/anime_planet/anime_data.csv", encoding="utf-8", index=False)
 
+
 def main():
     data = load_data()
-    process_anime_data(data)
-    process_charactes_data(data)
+    anime_df = to_df(data)    
+    process_charactes_data(anime_df[["url", "characters_data"]].to_dict("records"))
+    process_anime_data(anime_df)    
+
 
 if __name__ == "__main__":
     main()
