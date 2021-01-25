8b3151b1cf2f555875af19cb2adf0cd531c11ace
oliver rosario <oliverros42@gmail.com>
2021-01-24 21:11:18 -0400

    Remove score limit

--- scrap_data_mal/join_anime_codes_2.py
+++ scrap_data_mal/join_anime_codes_2.py
@@ -14,7 +14,6 @@ for csv_file in os.listdir(anime_codes_dir):
         genre_df = pd.read_csv(os.path.join(anime_codes_dir, csv_file))
         codes_df = codes_df.append(genre_df)
 
-codes_df = codes_df[codes_df["rating"] >= 6.5]
 codes_df.drop_duplicates(subset=['code'], inplace=True)
 
 codes_df.to_csv("../data/anime_codes.csv", index = False)
