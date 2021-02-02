bacf3637bee8f9662fd2a71e24dd79d31a63cbd4
oliver rosario <oliverros42@gmail.com>
2021-02-01 20:42:51 -0400

    Delete random recommender
    Since randomanime.org already has plenty random recommendation features
    this one will not be used

--- algorithms/__init__.py
+++ algorithms/__init__.py
@@ -3,12 +3,11 @@ from algorithms.soft_clustering import soft_clustering_recommendator
 from algorithms.synopsis_similarity import synopsis_similarity_recommender
 from algorithms.genre_match import genre_match_recommender
 from algorithms.character_match import character_match_recommender
-from algorithms.random import random_recommender
 
 recommender_algorithms = {
-    "similarity_search": similarity_recommendator,
+    "review_similarity": similarity_recommendator,
     "soft_clustering": soft_clustering_recommendator,
     "synopsis_similarity": synopsis_similarity_recommender,
-    "genre_match": genre_match_recommender,
-    "character_match": character_match_recommender,
-    "random": random_recommender}
+    "genre_similarity": genre_match_recommender,
+    "character_similarity": character_match_recommender,
+} 
